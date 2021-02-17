accc0929bc296a3c5d8ee791616fd8d7f51e9053
guimariz <guimariz@gmail.com>
2021-02-17 12:15:38 -0300

    get implementado

--- index.php
+++ index.php
@@ -4,11 +4,6 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . './vendor/autoload.php';
-
-$app = new \Slim\App();
-
-$app->get('/', function(Request $request, Response $response, array $args){
-  return $response->getBody()->write('Hello World');
-});
-
-$app->run();
+require __DIR__ . './env.php';
+require __DIR__ . './src/slimConfiguration.php';
+require __DIR__ . './routes/index.php';
